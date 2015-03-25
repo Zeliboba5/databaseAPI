d3ea4ab54058ee3f1d289149590c41588e219632
Andrey Bondar <Alanai1995@gmail.com>
2015-03-25 19:53:13 +0300

    new sqldump

--- SQLDUMP.sql
+++ SQLDUMP.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `TechDB` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `TechDB`;
 -- MySQL dump 10.13  Distrib 5.6.19, for osx10.7 (i386)
 --
 -- Host: 127.0.0.1    Database: TechDB
@@ -217,4 +219,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-03-25 19:08:55
+-- Dump completed on 2015-03-25 19:52:40
